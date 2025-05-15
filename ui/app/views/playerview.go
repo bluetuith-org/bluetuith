002969563474508a3e0a3d336660d936e54ee9af
darkhz <kmachanwenw@gmail.com>
2025-05-15 19:33:06 +0530

    ui/playerview: Optmize player drawing and event handling

--- ui/app/views/playerview.go
+++ ui/app/views/playerview.go
@@ -2,11 +2,12 @@ package views
 
 import (
 	"errors"
-	"go.uber.org/atomic"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+
+	"go.uber.org/atomic"
 
 	"github.com/bluetuith-org/bluetooth-classic/api/bluetooth"
 	"github.com/darkhz/bluetuith/ui/keybindings"
@@ -39,8 +40,6 @@ type playerElements struct {
 	info, title, progress, track, buttons *tview.TextView
 }
 
-const mediaButtons = `["rewind"][::b][<<][""] ["prev"][::b][<][""] ["play"][::b][|>][""] ["next"][::b][>][""] ["fastforward"][::b][>>][""]`
-
 // Initialize initializes the media player.
 func (m *mediaPlayer) Initialize() error {
 	m.isSupported.Store(true)
@@ -69,7 +68,7 @@ func (m *mediaPlayer) show() {
 	}
 
 	m.currentMedia = m.app.Session().MediaPlayer(device.Address)
-	_, err := m.currentMedia.Properties()
+	properties, err := m.currentMedia.Properties()
 	if err != nil {
 		m.status.ErrorMessage(err)
 		return
@@ -83,7 +82,7 @@ func (m *mediaPlayer) show() {
 		m.playerLock = semaphore.NewWeighted(1)
 	}
 
-	go m.updateLoop(device.Name)
+	go m.updateLoop(device, properties)
 }
 
 // close closes the media player.
@@ -101,33 +100,13 @@ func (m *mediaPlayer) close() {
 	}
 }
 
-// getProgress returns the title and progress of the currently playing media.
-func (m *mediaPlayer) getProgress(media bluetooth.MediaData, buttons string, width int, skip bool) (title string, newbuttons string, track string, progress string) {
+// renderProgress renders the player progress bar.
+func (m *mediaPlayer) renderProgress(progressView *tview.TextView, media bluetooth.MediaData) {
 	var length int
 
-	title = media.Title
+	_, _, width, _ := m.pages.GetRect()
 	position := media.Position
 	duration := media.Duration
-	number := strconv.FormatUint(uint64(media.TrackNumber), 10)
-	total := strconv.FormatUint(uint64(media.TotalTracks), 10)
-
-	button := "|>"
-	oldButton := button
-
-	if !skip {
-		switch media.Status {
-		case "playing":
-			button = "||"
-
-		case "paused":
-			button = "|>"
-
-		case "stopped":
-			button = "[]"
-		}
-	}
-
-	buttons = strings.Replace(buttons, oldButton, button, 1)
 
 	width /= 2
 	if position >= duration {
@@ -145,16 +124,89 @@ func (m *mediaPlayer) getProgress(media bluetooth.MediaData, buttons string, wid
 		endlength = width
 	}
 
-	track = "Track " + number + "/" + total
-	progress = " " + formatDuration(position) +
-		" |" + strings.Repeat("█", length) + strings.Repeat(" ", endlength) + "| " +
-		formatDuration(duration)
+	var sb strings.Builder
 
-	return title, buttons, track, progress
+	sb.WriteString(" ")
+	sb.WriteString(formatDuration(position))
+	sb.WriteString(" |")
+	sb.WriteString(strings.Repeat("█", length))
+	sb.WriteString(strings.Repeat(" ", endlength))
+	sb.WriteString("| ")
+	sb.WriteString(formatDuration(duration))
+
+	progressView.SetText(sb.String())
+}
+
+// renderButtons renders the player buttons.
+func (m *mediaPlayer) renderButtons(buttonsView *tview.TextView, mediaStatus bluetooth.MediaStatus, skip bool) {
+	const (
+		mediaLeftButtons  = `["rewind"][::b]<<[""] ["prev"][::b]<[""] ["play"][::b]`
+		mediaRightButtons = `[""] ["next"][::b]>[""] ["fastforward"][::b]>>[""]`
+	)
+
+	button := "|>"
+
+	if !skip {
+		switch mediaStatus {
+		case bluetooth.MediaPlaying:
+			button = "||"
+
+		case bluetooth.MediaPaused:
+			button = "|>"
+
+		case bluetooth.MediaStopped:
+			button = "[]"
+		}
+	}
+
+	var sb strings.Builder
+
+	sb.WriteString(mediaLeftButtons)
+	sb.WriteString(button)
+	sb.WriteString(mediaRightButtons)
+
+	buttonsView.SetText(sb.String())
+}
+
+// renderTrackData renders the track details.
+func (m *mediaPlayer) renderTrackData(infoView, titleView, trackView *tview.TextView, trackData bluetooth.TrackData) {
+	number := strconv.FormatUint(uint64(trackData.TrackNumber), 10)
+	total := strconv.FormatUint(uint64(trackData.TotalTracks), 10)
+
+	track := "Track " + number + "/" + total
+
+	titleView.SetText(trackData.Title)
+	infoView.SetText(trackData.Artist + " - " + trackData.Album)
+	trackView.SetText(track)
+}
+
+// renderPlayer renders the entire media player.
+func (m *mediaPlayer) renderPlayer(mediaEvent bluetooth.MediaData, cached *bluetooth.MediaEventData, elements playerElements) bool {
+	if mediaEvent.TrackData != (bluetooth.TrackData{}) && mediaEvent.TrackData != cached.TrackData {
+		cached.TrackData = mediaEvent.TrackData
+
+		m.renderTrackData(elements.info, elements.title, elements.track, cached.TrackData)
+	}
+
+	if mediaEvent != (bluetooth.MediaData{}) && mediaEvent != cached.MediaData {
+		if mediaEvent.Position != cached.Position {
+			cached.Position = mediaEvent.Position
+
+			m.renderProgress(elements.progress, cached.MediaData)
+		}
+
+		if mediaEvent.Status != "" && mediaEvent.Status != cached.Status {
+			cached.Status = mediaEvent.Status
+
+			m.renderButtons(elements.buttons, mediaEvent.Status, m.skip.Load())
+		}
+	}
+
+	return true
 }
 
 // updateLoop updates the media player.
-func (m *mediaPlayer) updateLoop(deviceName string) {
+func (m *mediaPlayer) updateLoop(device bluetooth.DeviceData, props bluetooth.MediaData) {
 	if !m.playerLock.TryAcquire(1) {
 		return
 	}
@@ -180,51 +232,49 @@ func (m *mediaPlayer) updateLoop(deviceName string) {
 	t := time.NewTicker(1 * time.Second)
 	defer t.Stop()
 
+	var cachedMediaData bluetooth.MediaEventData
+
+	m.renderPlayer(props, &cachedMediaData, elements)
+
 PlayerLoop:
 	for {
-		media, err := m.currentMedia.Properties()
-		if err != nil {
-			break PlayerLoop
-		}
-
-		_, _, width, _ := m.pages.GetRect()
-		title, buttons, tracknum, progress := m.getProgress(media, mediaButtons, width, m.skip.Load())
-
-		m.app.InstantDraw(func() {
-			elements.info.SetText(media.Artist + " - " + media.Album)
-			elements.title.SetText(title)
-			elements.track.SetText(tracknum)
-			elements.buttons.SetText(buttons)
-			elements.progress.SetText(progress)
-		})
-
 		select {
 		case <-m.stopEvent:
 			break PlayerLoop
 
-		case highlight, ok := <-m.keyEvent:
-			if !ok {
-				break PlayerLoop
-			}
-
-			elements.buttons.Highlight(highlight)
+		case highlight := <-m.keyEvent:
+			go m.app.QueueDraw(func() {
+				elements.buttons.Highlight(highlight)
+			})
 			t.Reset(1 * time.Second)
 
 		case <-m.buttonEvent:
+			go m.app.QueueDraw(func() {
+				m.renderButtons(elements.buttons, cachedMediaData.Status, m.skip.Load())
+			})
 			t.Reset(1 * time.Second)
 
-		case _, ok := <-mediaSub.C:
+		case mediaEvent, ok := <-mediaSub.C:
 			if !ok {
 				break PlayerLoop
 			}
 
-			t.Reset(1 * time.Second)
+			if mediaEvent.Data.Address != device.Address {
+				continue
+			}
+
+			go m.app.QueueDraw(func() {
+				if m.renderPlayer(mediaEvent.Data.MediaData, &cachedMediaData, elements) {
+					t.Reset(1 * time.Second)
+				}
+			})
 
 		case <-t.C:
+			mediaData := cachedMediaData.MediaData
 			if cachedMediaData.Status == bluetooth.MediaPlaying {
 				cachedMediaData.Position = cachedMediaData.Position + 1000
-				m.app.QueueDraw(func() {
-					m.renderProgress(elements.progress, cachedMediaData.MediaData)
+				go m.app.QueueDraw(func() {
+					m.renderProgress(elements.progress, mediaData)
 				})
 			}
 		}
@@ -266,13 +316,13 @@ func (m *mediaPlayer) setup(deviceName string) playerElements {
 
 	buttons := tview.NewTextView()
 	buttons.SetRegions(true)
-	buttons.SetText(mediaButtons)
 	buttons.SetDynamicColors(true)
 	buttons.SetTextAlign(tview.AlignCenter)
 	buttons.SetTextColor(theme.GetColor(theme.ThemeText))
 	buttons.SetBackgroundColor(theme.GetColor(theme.ThemeBackground))
 	buttons.SetHighlightedFunc(func(added, _, _ []string) {
 		var key keybindings.Key
+		var keepHighlight bool
 
 		if added == nil {
 			return
@@ -289,21 +339,31 @@ func (m *mediaPlayer) setup(deviceName string) playerElements {
 			key = keybindings.KeyPlayerPrevious
 
 		case "fastforward":
-			buttons.Highlight(added[0])
+			keepHighlight = true
 			key = keybindings.KeyPlayerSeekForward
 
 		case "rewind":
-			buttons.Highlight(added[0])
+			keepHighlight = true
 			key = keybindings.KeyPlayerSeekBackward
 
 		default:
 			return
 		}
 
+		if !keepHighlight {
+			go func() {
+				time.Sleep(250 * time.Millisecond)
+				m.app.QueueDraw(func() {
+					buttons.Highlight("")
+				})
+			}()
+		}
+
 		keyData := m.kb.Data(key)
 
 		m.keyEvents(tcell.NewEventKey(keyData.Kb.Key, keyData.Kb.Rune, keyData.Kb.Mod), true)
 	})
+	m.renderButtons(buttons, bluetooth.MediaPlaying, false)
 
 	buttonFlex := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
@@ -335,34 +395,34 @@ func (m *mediaPlayer) keyEvents(event *tcell.EventKey, button bool) {
 		return
 	}
 
-	var highlight string
 	var nokey bool
+	var highlight string
 
-	operation := m.kb.Key(event)
-
-	switch operation {
+	switch m.kb.Key(event, keybindings.ContextDevice) {
 	case keybindings.KeyPlayerSeekForward:
-		m.currentMedia.FastForward()
-
-		m.skip.Store(true)
 		highlight = "fastforward"
+		m.currentMedia.FastForward()
+		m.skip.Store(true)
 
 	case keybindings.KeyPlayerSeekBackward:
-		m.currentMedia.Rewind()
-
-		m.skip.Store(true)
 		highlight = "rewind"
+		m.currentMedia.Rewind()
+		m.skip.Store(true)
 
 	case keybindings.KeyPlayerPrevious:
+		highlight = "prev"
 		m.currentMedia.Previous()
 
 	case keybindings.KeyPlayerNext:
+		highlight = "next"
 		m.currentMedia.Next()
 
 	case keybindings.KeyPlayerStop:
+		highlight = "play"
 		m.currentMedia.Stop()
 
 	case keybindings.KeyPlayerTogglePlay:
+		highlight = "play"
 		if m.skip.Load() {
 			m.currentMedia.Play()
 			m.skip.Store(false)
@@ -397,7 +457,7 @@ func (m *mediaPlayer) keyEvents(event *tcell.EventKey, button bool) {
 
 // formatDuration converts a duration into a human-readable format.
 func formatDuration(duration uint32) string {
-	var durationtext string
+	var durationtext strings.Builder
 
 	input, err := time.ParseDuration(strconv.FormatUint(uint64(duration), 10) + "ms")
 	if err != nil {
@@ -416,30 +476,30 @@ func formatDuration(duration uint32) string {
 
 	if h > 0 {
 		if h < 10 {
-			durationtext += "0"
+			durationtext.WriteString("0")
 		}
 
-		durationtext += strconv.Itoa(int(h))
-		durationtext += ":"
+		durationtext.WriteString(strconv.Itoa(int(h)))
+		durationtext.WriteString(":")
 	}
 
 	if m > 0 {
 		if m < 10 {
-			durationtext += "0"
+			durationtext.WriteString("0")
 		}
 
-		durationtext += strconv.Itoa(int(m))
+		durationtext.WriteString(strconv.Itoa(int(m)))
 	} else {
-		durationtext += "00"
+		durationtext.WriteString("00")
 	}
 
-	durationtext += ":"
+	durationtext.WriteString(":")
 
 	if s < 10 {
-		durationtext += "0"
+		durationtext.WriteString("0")
 	}
 
-	durationtext += strconv.Itoa(int(s))
+	durationtext.WriteString(strconv.Itoa(int(s)))
 
-	return durationtext
+	return durationtext.String()
 }
